4ab04fcf273e72e9811f3baa23f7c002e86a5e96
Verunka <benveronika@seznam.cz>
2022-04-13 19:57:38 +0200

    želvička pokus o kopii

--- src/main/java/net/sevecek/turtle/HlavniProgram.java
+++ src/main/java/net/sevecek/turtle/HlavniProgram.java
@@ -2,12 +2,39 @@ package net.sevecek.turtle;
 
 import net.sevecek.turtle.engine.*;
 
+import java.awt.*;
+
 public class HlavniProgram {
 
     public void main(String[] args) {
         Turtle zofka;
 
         zofka = new Turtle();
+
+
+
+
+
+
+
+
+        zofka.turnRight(45);
+        zofka.move(50);
+        zofka.turnRight(90);
+        zofka.move(50);
+        zofka.turnRight(45);
+        zofka.move(50);
+        zofka.turnRight(90);
+        zofka.move(70);
+        zofka.turnRight(90);
+        zofka.move(50);
+        zofka.turnRight(90);
+        zofka.move(70);
+
+
+
+
+
     }
 
 }
